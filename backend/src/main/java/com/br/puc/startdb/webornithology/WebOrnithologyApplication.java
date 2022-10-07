dc3e24eafef48e20e488f5aa74f89535d24b4e13
luh <luisa.santos@db.tec.br>
2022-10-07 14:26:58 -0300

    retirar data e local de pássaro. Mais dados no bd

--- backend/src/main/java/com/br/puc/startdb/webornithology/WebOrnithologyApplication.java
+++ backend/src/main/java/com/br/puc/startdb/webornithology/WebOrnithologyApplication.java
@@ -7,14 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 @SpringBootApplication
-public class WebornithologyApplication {
+public class WebOrnithologyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebornithologyApplication.class, args);
+		SpringApplication.run(WebOrnithologyApplication.class, args);
 	}
 	
 
@@ -22,23 +20,28 @@ public class WebornithologyApplication {
 	 @Bean
 	  CommandLineRunner run(PassaroRepository repository) {
 		 return args -> {
-			 repository.save(new Passaro(null, "C:\\Users\\dlar-\\Downloads\\blue.jpg",
-					 "Arara-azul-grande",
-					 "Anodorhynchus hyacinthinus", "100 cm", "Anodorhynchus",
-					 "Azul", "Psittacidae", "tropical", "campus da pucRS",
-					 LocalDate.of(2022, 9, 29), LocalTime.of(14, 33)));
+			 repository.save(new Passaro(null, "https://s3.amazonaws.com/media.wikiaves.com.br/images/0721/1270591g_6ab6166e946550bd953a5e271ef70c1e.jpg",
+					 "cabeça-seca", "Wood Stork", "Mycteria americana Linnaeus", "65cm",
+					 "indeterminado", "branco", "Família Ciconiidae",
+					 "campo alagado, campo úmido, várzeas alagadas, banhado com espelho d’água"));
 
-			 repository.save(new Passaro(null, "C:\\Users\\dlar-\\Downloads\\tucano.jpg",
-					 "Tucano", "Ramphastidae", "61 cm",
-					 "Ramphastidae", "Preto", "Ramphastidae",
-					 "matas de galeria, cerrado e capões", "Jardim Botânico de Porto Alegre",
-					 LocalDate.of(2022, 8, 5), LocalTime.of(15, 0)));
+			 repository.save(new Passaro(null, "https://s3.amazonaws.com/media.wikiaves.com.br/images/7505/5057168g_b0e8a72ac401be29c56c91b085e9114b.jpg",
+					 "joão-grande", "Maguari Stork", "Ciconia maguar", "85cm",
+					 "indeterminado", "branco", "Família Ciconiidae",
+					 "campo alagado, campo úmido, várzeas alagadas, banhado com espelho d’água"));
+			 repository.save(new Passaro(null, "https://s3.amazonaws.com/media.wikiaves.com.br/images/6005/5006384g_bf025625d3572b322238722a2894b094.jpg",
+					 "cisne-de-pescoço-preto", "Black-necked-swan", "Cygnus melancoryphus", "80cm",
+					 "Indeterminado", "branco", "Família Anatidae",
+					 "campo alagado, campo úmido, várzeas alagadas, banhado com espelho d’água"));
+			 repository.save(new Passaro(null, "https://s3.amazonaws.com/media.wikiaves.com.br/images/3505/5053290g_ce4f005d8d44f55fdffa65308b564397.jpg",
+					 "capororoca", "Coscoroba Swan", "Cosocoroba coscoroba", "65cm",
+					 "Indeterminado", "branco", "Família Anatidae",
+					 "campo alagado, campo úmido, várzeas alagadas, banhado com espelho d’água"));
+			 repository.save(new Passaro(null, "https://s3.amazonaws.com/media.wikiaves.com.br/images/4605/5064595_ab0881bb6d108ec1669743ff8b9af9fa.jpg",
+					 "Great Egret", "Great Egret", "Ardea Alba", "65cm",
+					 "Indeterminado", "branco", "Família Ardeidae",
+					 "campo alagado, campo úmido, várzeas alagadas, banhado com espelho d’água"));
 
-			 repository.save(new Passaro(null, "C:\\Users\\dlar-\\Downloads\\Woodpecker.jpeg",
-					 "Pica Pau",
-					 "Campephilus melanoleucos malherbii", "36 cm", "Picides",
-					 "Preto", "Picidae", "tropical", "Parque Redenção, Porto Alegre",
-					 LocalDate.of(2022, 6, 15), LocalTime.of(16, 45)));
 		 };
 	 }
 
