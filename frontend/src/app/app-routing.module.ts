a268c956338552ff5d6b883741f6632ff9e8ac62
luh <luisa.santos@db.tec.br>
2022-10-27 09:46:51 -0300

    routing get e página passaro

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './Components/home/home.component';
 import { TelaListagemComponent } from './Components/tela-listagem/tela-listagem.component';
 import { TelaPesquisaComponent } from './Components/tela-pesquisa/tela-pesquisa.component';
 import { TelaPostagemComponent } from './Components/tela-postagem/tela-postagem.component';
+import { TelaPassaroComponent } from './Components/tela-passaro/tela-passaro.component';
 
 const routes: Routes = [
     { path:'', component: HomeComponent },
     { path:'list', component: TelaListagemComponent},
     { path:'post', component: TelaPostagemComponent },
-    { path:'search', component: TelaPesquisaComponent }
+    { path:'search', component: TelaPesquisaComponent },
+    { path:'get', component: TelaPassaroComponent }
 ];
 
 @NgModule({
