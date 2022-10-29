f697eac0ca5b9715508c4ae79392f0a397fd2e0e
drodrigues17 <adanielluiz71@gmail.com>
2022-10-29 18:50:52 -0300

    versao para pareamento

--- backend/src/main/java/com/br/puc/startdb/webornithology/WebOrnithologyApplication.java
+++ backend/src/main/java/com/br/puc/startdb/webornithology/WebOrnithologyApplication.java
@@ -2,13 +2,10 @@ package com.br.puc.startdb.webornithology;
 
 import com.br.puc.startdb.webornithology.model.Passaro;
 import com.br.puc.startdb.webornithology.repository.PassaroRepository;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
--- frontend/src/app/Components/tela-postagem/tela-postagem.component.ts
+++ frontend/src/app/Components/tela-postagem/tela-postagem.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { Passaro } from 'src/app/interface/Passaro';
+import { PassaroService } from 'src/app/service/PassaroService';
 
 @Component({
   selector: 'app-tela-postagem',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelaPostagemComponent implements OnInit {
 
-  constructor() { }
+  passaro!: Passaro;
+
+  
+
+  constructor(
+    public passaroService: PassaroService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  realizarCadastro(): void{
+    this.passaro.linkImagem = '';
+    this.passaro.nome ="";
+    this.passaro.nomeIngles ="";
+    this.passaro.nomeLatin ="";
+    this.passaro.tamanho ="";
+    this.passaro.genero ="";
+    this.passaro.corPredominante ="";
+    this.passaro.familia ="";
+    this.passaro.habitat ="";
+    console.log(this.passaro);
+  }
 }
